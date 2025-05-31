11d2e8267b57cb189741762cf10c341004f717d9
Beatriz Messias <beatrizmessiascr@gmail.com>
2025-05-31 18:54:26 -0300

    feat: add tie logic

--- script.js
+++ script.js
@@ -1,20 +1,85 @@
 const Tictactoe = () => {
+    let canPlay = false, game = [], board;
     const texts = [
         "Press to play",
         "plays!",
-        "won"
+        "won!",
+        "Tie!"
     ];
     const self = {
         bot: true,
         turn: "",
-        text: texts[0]
+        text: texts[0],
+        buttonLabel: "Play"
     };
 
+    const checkMatching = (val1, val2, val3) => {
+        if (game[val1] === game[val2] && game[val2] === game[val3]) {
+            return game[val1];
+        }
+    }
+
+    const isBoardFull = () => {
+        for (let i = 1; i <= 9; i++) {
+            if (!game[i]) return false;
+        }
+        return true;
+    }
+
+    const clickedBox = (element) => {
+        const id = element.getAttribute("data-id");
+        if (!canPlay || game[id])
+            return false;
+
+        element.innerText = self.turn;
+        game[id] = self.turn;
+
+        if (self.turn === "x") {
+            self.turn = "o";
+        }
+        else {
+            self.turn = "x";
+        }
+
+        const winner = checkMatching(1, 2, 3) ||
+                       checkMatching(4, 5, 6) ||
+                       checkMatching(7, 8, 9) ||
+                       checkMatching(1, 4, 7) ||
+                       checkMatching(2, 5, 8) ||
+                       checkMatching(3, 6, 9) ||
+                       checkMatching(1, 5, 9) ||
+                       checkMatching(3, 5, 7);
+
+        if (winner) {
+            self.turn = winner;
+            self.text = texts[2];
+            canPlay = false;
+        } else if (isBoardFull()) {
+            self.turn = "";
+            self.text = texts[3];
+            canPlay = false;
+        }
+
+        return true;
+    }
+
     self.init = (elem) => {
+        board = elem;
         elem.addEventListener("click", (e) => {
             switch(e.target.tagName) {
                 case "SPAN":
-                    console.log("cell");
+                    if (clickedBox(e.target)) {
+                        if (self.bot) {
+                            elem.style.pointerEvents = "none";
+
+                            setTimeout(() => {
+                                const emptyTiles = elem.querySelectorAll("span:empty");
+                                const cell = emptyTiles[Math.floor(Math.random() * emptyTiles.length)];
+                                clickedBox(cell);
+                                elem.style.pointerEvents = "";
+                            }, 500);
+                        }
+                    };
                 break;
                 case "BUTTON":
                     play();
@@ -24,22 +89,30 @@ const Tictactoe = () => {
     }
 
     const play = () => {
-        self.turn = "";
+        canPlay = true;
+        self.turn = "x";
         self.text = texts[1];
+        game = [];
+
+        const cells = board.querySelectorAll("span");
+        for (let i = 0; i < cells.length; i++) {
+            cells[i].innerText = "";
+        }
+        self.buttonLabel = "Restart";
     }
 
     const template = `
         <div>
             <h1>Tic Tac Toe</h1>
             <p>
-                Play against the computer <input type="checkbox" checked onchange="self.bot = this.checked">
+                Play against the computer <input type="checkbox" checked @bind="self.bot">
             </p>
             <div>
                 <div class="gui">
                     <span class="gui__turn">{{self.turn}}</span>
                     <span>{{self.text}}</span>
                 </div>
-                <div class="board" oncreate="self.init(this)">
+                <div class="board" @ready="self.init(this)">
                     <section class="board__column">
                         <span class="board__cell" data-id="1"></span>
                         <span class="board__cell" data-id="2"></span>
@@ -55,7 +128,7 @@ const Tictactoe = () => {
                         <span class="board__cell" data-id="8"></span>
                         <span class="board__cell" data-id="9"></span>
                     </section>
-                    <button class="btn">Play</button>
+                    <button class="btn">{{self.buttonLabel}}</button>
                 </div>
             </div>
         </div>
